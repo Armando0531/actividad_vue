2eff273005ff127907673ff26c5ba5fbb0d2413d
Armando <armandoenriquez0531@gmail.com>
2024-02-16 14:18:41 -0600

    Configuramos la aplicacion vue para utilizar vuex para la gestion del estado y vue router para la navegacion entre diferentes vistas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,53 @@
 import './assets/main.css'
 
+import { createStore } from 'vuex'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const store = createStore({
+    state: {
+      no_control: '',
+      nommbre: '',
+      edad: '',
+      semestre: '',
+      carrera: '',
+      mat_agregada: '',
+      mat_especial: '',
+      act_complementaria: '',
+    },
+    mutations: {
+      enviarNoControl(state, nomc) {
+          state.no_control = nomc
+      },
+      enviarNombre(state, numc) {
+          state.nommbre = numc
+      },
+      enviarEdad(state, ed) {
+          state.edad = ed
+      },
+      enviarSemestre(state, sem) {
+          state.semestre = sem
+      },
+      enviarCarrera(state, ca) {
+          state.carrera = ca
+      },
+      enviarMat_agregada(state, matagr) {
+          state.mat_agregada = matagr
+      },
+      enviarMat_especial(state, matesp) {
+          state.mat_especial = matesp
+      },
+      enviarAct_complementariap(state, actext) {
+          state.act_complementaria = actext
+      }
+    }
+    
+  });
+
 app.use(router)
+app.use(store)
 
 app.mount('#app')
